0199df9dd6934fac999a9a82d40bf2e145196610
adermyn <123884328+adermyn@users.noreply.github.com>
2023-04-18 12:50:36 +0000

    тест

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -4,7 +4,7 @@ const Input = styled.input`
     outline: none;
     border-radius: 0px;
     border: 1px solid #E2E8F0;
-    border-radius: 24px;
+    border-radius: 23px;
     background-color: #F8FAFC;
     color: #666787;
     &:hover {
